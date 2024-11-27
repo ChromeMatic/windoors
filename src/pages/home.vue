92e2767fde8d41156e9e562f4372086375254b19
ChromeMatic <armanibrwn@gmail.com>
2024-11-27 11:43:05 -0500

    Contact Infomation added

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -9,16 +9,15 @@
    meta:[
      {
        name: 'description',
-       content: "Looking for top-quality windows and glass door solutions? At International Windoors & Glass Solutions, we offer beautifully crafted windows and glass doors designed to elevate the aesthetics and functionality of any space. Our custom solutions are perfect for homes, offices, and commercial properties, combining innovative design with exceptional craftsmanship. From energy-efficient windows to modern glass door installations, our products are built to last and enhance your living or working environment. We specialize in bespoke designs tailored to your specific needs, ensuring that every project is completed to the highest standards. Whether you need sleek sliding doors, stunning glass facades, or durable window installations, our expert team provides efficient service and lasting results. Transform your space with premium windows and glass doors from International Windoors & Glass Solutions — your trusted partner in design, quality, and performance.\n" +
-           "\n" +
-           "Key Features:\n" +
-           "\n" +
-           "Custom Windows & Glass Door Solutions\n" +
-           "Energy-Efficient & Sustainable Products\n" +
-           "Expert Craftsmanship & Precision Design\n" +
-           "Residential, Commercial & Industrial Installations\n" +
-           "Seamless Service & Reliable Performance\n" +
-           "Let us bring your vision to life with high-quality window and glass door solutions that blend style, functionality, and durability.",
+       content: "Looking for top-quality windows and glass door solutions? At International Windoors & Glass Solutions, we offer beautifully crafted windows and glass doors designed to elevate the aesthetics and functionality of any space. Our custom solutions are perfect for homes, offices, and commercial properties, combining innovative design with exceptional craftsmanship. From energy-efficient windows to modern glass door installations, our products are built to last and enhance your living or working environment. We specialize in bespoke designs tailored to your specific needs, ensuring that every project is completed to the highest standards. Whether you need sleek sliding doors, stunning glass facades, or durable window installations, our expert team provides efficient service and lasting results. Transform your space with premium windows and glass doors from International Windoors & Glass Solutions — your trusted partner in design, quality, and performance." +
+           +"Key Features: "+
+           " Custom Windows & Glass Door Solutions," +
+           " Energy-Efficient & Sustainable Products," +
+           " Expert Craftsmanship & Precision Design," +
+           " Residential, Commercial & Industrial Installations," +
+           " Seamless Service & Reliable Performance," +
+           " Let us bring your vision to life with high-quality window and glass door solutions that blend style, " +
+           " functionality, and durability.",
      }
    ]
  })
@@ -35,6 +34,13 @@
    { src:'/IMG_20220520_113039_687.jpg', name:"IMG_20220520_113039_687.jpg" },
    { src:'/IMG_20220812_081852_823.jpg', name:"IMG_20220812_081852_823.jpg" },
  ]
+
+ let ContactInfo ={
+   phone_number1: '(876) 470-2676',
+   phone_number2: '(876) 481-3881',
+   email_address1: 'devonbarrett456@gmail.com',
+   email_address2: 'thomasdwane16@gmail.com',
+ }
 </script>
 
 <template>
@@ -155,6 +161,59 @@
 
       </div>
 
+    </div>
+
+    <div
+        class="flex flex-col justify-center items-center w-full space-y-6 lg:p-6 p-4 rounded-2xl shadow-lg border-2"
+        :class="useTheme() ? 'bg-innerDark border-teal-950 shadow-lg shadow-teal-900':' bg-Snow shadow-teal-300 border-teal-100'"
+    >
+          <h2 class="text-3xl overline font-semibold">
+            Contact Information
+          </h2>
+
+          <div
+              class="flex space-x-2 justify-center items-center w-full"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+              <path d="M10.5 18.75a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z" />
+              <path fill-rule="evenodd" d="M8.625.75A3.375 3.375 0 0 0 5.25 4.125v15.75a3.375 3.375 0 0 0 3.375 3.375h6.75a3.375 3.375 0 0 0 3.375-3.375V4.125A3.375 3.375 0 0 0 15.375.75h-6.75ZM7.5 4.125C7.5 3.504 8.004 3 8.625 3H9.75v.375c0 .621.504 1.125 1.125 1.125h2.25c.621 0 1.125-.504 1.125-1.125V3h1.125c.621 0 1.125.504 1.125 1.125v15.75c0 .621-.504 1.125-1.125 1.125h-6.75A1.125 1.125 0 0 1 7.5 19.875V4.125Z" clip-rule="evenodd" />
+            </svg>
+            <h2>Telephone Number:</h2>
+            <h2 class="font-bold transition-all ease-in-out duration-700 hover:text-indigo-500">
+              {{ ContactInfo.phone_number1 }} | {{ ContactInfo.phone_number2 }}
+            </h2>
+          </div>
+
+      <div
+          class="flex space-x-2 justify-center items-center w-full"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+          <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
+          <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
+        </svg>
+        <h2> Email Address:
+         <a
+             class="font-bold transition-all ease-in-out duration-700 hover:text-indigo-500"
+             :href="ContactInfo.email_address1"
+             target="_blank"
+             rel="noopener noreferrer"
+         >
+           {{ ContactInfo.email_address1 }}
+         </a>
+
+         |
+
+         <a
+             class="font-bold transition-all ease-in-out duration-700 hover:text-indigo-500"
+             :href="ContactInfo.email_address2"
+             target="_blank"
+             rel="noopener noreferrer"
+         >
+           {{ ContactInfo.email_address2 }}
+         </a>
+
+       </h2>
+      </div>
     </div>
 
 
